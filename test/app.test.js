1d445175ec1c6d62b20df3491135c0db4eaa2195
Pritiam Gurung <pritim135@gmail.com>
2026-01-28 17:13:58 -0500

    Formate code with Prettier

--- test/app.test.js
+++ test/app.test.js
@@ -1,20 +1,18 @@
-const request = require('supertest');
-const app = require('../src/app');
+const request = require("supertest");
+const app = require("../src/app");
 
-describe('GET /health', () => {
-
+describe("GET /health", () => {
   test('returns status 200 and status === "healthy"', async () => {
-    const response = await request(app).get('/health');
+    const response = await request(app).get("/health");
 
     expect(response.status).toBe(200);
-    expect(response.body.status).toBe('healthy');
+    expect(response.body.status).toBe("healthy");
   });
 
-  test('uptime exists and is a number', async () => {
-    const response = await request(app).get('/health');
+  test("uptime exists and is a number", async () => {
+    const response = await request(app).get("/health");
 
-    expect(response.body).toHaveProperty('uptime');
-    expect(typeof response.body.uptime).toBe('number');
+    expect(response.body).toHaveProperty("uptime");
+    expect(typeof response.body.uptime).toBe("number");
   });
-
 });
